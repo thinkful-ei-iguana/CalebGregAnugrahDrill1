a79b0d83b78a69a96f371eb301ee6524cb946446
Greg <grachkovsky@gmail.com>
2019-10-21 17:11:38 -0400

    added born in year

--- Drill.js
+++ Drill.js
@@ -1,8 +1,8 @@
 /* eslint-disable strict */
 function createGreeting(name, age) {
-  return `Hi my name is ${name} and I'm ${age} years old`;
+  let yearOfBirth = 2019 - age;
+  return `Hi my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
 const greeting1 = createGreeting();
 console.log(greeting1);
-
