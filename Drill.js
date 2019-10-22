f74ec9378be3493ff499b26de324bed00e0625dd
Caleb King <caleb.king536@gmail.com>
2019-10-21 18:29:01 -0600

    completed try catch blocks

--- Drill.js
+++ Drill.js
@@ -4,8 +4,24 @@ function getYearOfBirth(age) {
 }
 
 function createGreeting(name, age) {
+  if (name === undefined || age === undefined) {
+    throw new Error('Arguments not valid');
+  }
+  
+  if (age < 0) {
+    throw new Error('Age cannot be negative.');
+  }
+
+  if (typeof age !== 'number') {
+    throw new TypeError('Age must be a number');
+  }
+  
   return `Hi my name is ${name} and I'm ${age} years old. I was born in ${getYearOfBirth(age)}.`;
 }
 
-const greeting1 = createGreeting();
-console.log(greeting1);
+try {
+  const greeting1 = createGreeting('Caleb', 27);
+  console.log(greeting1);
+} catch(e) {
+  console.error(e.message);
+}
